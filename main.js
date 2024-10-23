b36c96f2c5b8e50ad686ed4bd9078bbd421800f3
SvitlanaTurych bc2024-4 <svitlana.study24@gmail.com>
2024-10-23 19:32:15 +0300

    command GET

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const { Command } = require('commander');
-const fs = require('fs');
+const fs = require('fs').promises; 
 const path = require('path');
 
 const program = new Command();
@@ -14,13 +14,32 @@ program.parse(process.argv);
 
 const options = program.opts();
 
-if (!fs.existsSync(options.cache)) {
-  console.error(`Error: Cache directory "${options.cache}" does not exist.`);
-  process.exit(1);
-}
+fs.access(options.cache)
+  .catch(() => {
+    console.error(`Error: Cache directory "${options.cache}" does not exist.`);
+    process.exit(1);
+  });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+const server = http.createServer(async (req, res) => {
+  const urlPath = req.url;
+  const method = req.method;
+ 
+  const code = urlPath.slice(1);
+  const filePath = path.join(options.cache, `${code}.jpg`);
+
+  if (method === 'GET') {
+    try {
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('405 Method Not Allowed');
+  }
 });
 
 server.listen(options.port, options.host, () => {
